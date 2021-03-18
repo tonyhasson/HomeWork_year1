5555a6ff226d7c47556aec50b318b6c7d03131ad
tonyhasson <63504193+tonyhasson@users.noreply.github.com>
2021-03-18 17:32:04 +0200

    Create main_matrix.cpp

---
+++ semester_b/OOP/Extras/main_matrix.cpp
@@ -0,0 +1,32 @@
+#include "Matrix.h"
+
+
+void main()
+{
+	//Matrix m1,m2,m3,m4;
+	//cin >> m1;
+	//cin >> m2;
+	//cout <<m1<<m2;
+	///*m1 += m2;
+	//cout << m1;
+	//m3 = m1 + m2;
+	//cout << m3;*/
+	//m3 = 5 * m2;
+	//cout << m3;
+	//m3[1][1]=2;
+	//cout << m3;
+	//m4 = m3 ^ 4;
+	//cout << m4;
+	Matrix m(2, 2);
+	m[0][0] = 2;
+	m[1][1] = 2;
+	cout << m << endl;
+	m = m;
+	const Matrix s = -m;
+	cout << m << endl << s << endl;
+	m = s + 2 * -m * m * 2 - s;
+	cout << m << endl << s << endl;
+	cout << s[1][1] << endl;
+	
+	
+}
