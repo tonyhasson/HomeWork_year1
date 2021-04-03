0dd3163b3aeb75322abe0feff5aa5fe87700089d
tonyhasson <63504193+tonyhasson@users.noreply.github.com>
2021-04-03 11:27:30 +0300

    Update ClassWork6.c

--- semester_b/advanced_prog/ClassWork6.c
+++ semester_b/advanced_prog/ClassWork6.c
@@ -1,59 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+#include <assert.h>
+
+
+
 typedef struct
 {
 	int num;
 	struct Node* n;
 }Node;
 
-int enter_numbers(Node* l1, Node* l2)
+int enter_numbers(Node** l1, Node** l2)
 {
-	int input,count=0;
-	Node* previous1;
-	Node* previous2;
+	int input, count = 0;
+	Node* head1;
+	Node* head2;
 	Node* temp1;
 	Node* temp2;
-	temp1 = NULL;
-	temp2 = NULL;
-	previous1 = NULL;
-	previous2 = NULL;
+	head1 =(Node*)malloc(sizeof(Node));
+	head2 = (Node*)malloc(sizeof(Node));
+	temp1 =head1;
+	temp2 = head2;
 	printf("enter a number\n");
 	scanf_s("%d", &input);
-	while (input!=-1)
+	while (input != -1)
 	{
 		count++;
 		if (input % 2 == 0)
 		{
-			if (temp2 == NULL)
+			if (temp2->n == NULL)
 			{
-				l2->num = input;
-				temp2= (Node*)malloc(sizeof(Node));
-				l2->n = temp2;
-				previous2 = l2;
-
-			}
-			else
-			{
-				temp2->num = input;
 				temp2->n = (Node*)malloc(sizeof(Node));
-				previous2 = temp2;
 				temp2 = temp2->n;
 			}
+			temp2->num = input;
+			temp2->n = NULL;
+
 		}
 		else
 		{
-			if (temp1 == NULL)
+			if (temp1->n == NULL)
 			{
-				l1->num = input;
-				temp1 = (Node*)malloc(sizeof(Node));
-				l1->n = temp1;
-				previous1 = l1;
-			}
-			else
-			{
-				temp1->num = input;
 				temp1->n = (Node*)malloc(sizeof(Node));
-				previous1 = temp1;
 				temp1 = temp1->n;
 			}
+			temp1->num = input;
+			temp1->n = NULL;
 		}
 
 
@@ -61,24 +57,20 @@ int enter_numbers(Node* l1, Node* l2)
 		printf("enter a number\n");
 		scanf_s("%d", &input);
 	}
-	if (temp1 == NULL)
+	if (temp2->n != NULL)
 	{
-		l1= NULL;
+		temp2 = NULL;
+		head2 = NULL;
 	}
-	else
+	if (temp1->n != NULL)
 	{
-		previous1->n = NULL;
+		temp1 = NULL;
+		head1 = NULL;
 	}
-	if (temp2 == NULL)
-	{
-		l2 = NULL;
-	}
-	else
-	{
-		previous2->n = NULL;
-	}
+	*l1 = head1;
+	*l2 = head2;
 	return(count);
-	   
+
 }
 void print_list(Node* head)
 {
@@ -89,25 +81,34 @@ void print_list(Node* head)
 		temp = temp->n;
 	}
 }
-void delete_list(Node* head)
+void delete_list(Node** head)
 {
-	Node* previous = NULL;
-	Node* temp = head->n;
+	
+	Node* next = NULL;
+	Node* temp = *head;
 	while (temp)
 	{
-		previous = temp;
-		temp = temp->n;
-		free(previous);
+		next = temp->n;
+		free(temp);
+		temp = next;
 	}
-	free(head);
+	*head = NULL;
 }
-int sum_list(Node* head,int* max,int* min)
+int sum_list(Node* head, int* max, int* min)
 {
 	Node* temp;
 	temp = head;
-	int sum=0;
-	*max = temp->num;
-	*min = temp->num;
+	int sum = 0;
+	if (temp)
+	{
+		*max = temp->num;
+		*min = temp->num;
+	}
+	else
+	{
+		*max = -1;
+		*min = -1;
+	}
 	while (temp)
 	{
 		sum += temp->num;
@@ -128,9 +129,10 @@ int sum_list(Node* head,int* max,int* min)
 
 void main()
 {
-	Node* l1 = (Node*)malloc(sizeof(Node));
-	Node* l2 = (Node*)malloc(sizeof(Node));
-	int count = enter_numbers(l1, l2),sum,max,min;
+	
+	Node* l1 ;
+	Node* l2;
+	int count = enter_numbers(&l1, &l2), sum, max, min;
 	printf("\n\n");
 	printf("amount of numbers entered is : %d\n", count);
 	printf("\n\n");
@@ -138,11 +140,12 @@ void main()
 	printf("\n\n");
 	print_list(l2);
 	printf("\n\n");
-	sum = sum_list(l1,&max,&min);
+	sum = sum_list(l1, &max, &min);
 	printf("the summary of l1 is: %d\nmin is: %d\nmax is %d", sum, min, max);
 	printf("\n\n");
 	sum = sum_list(l2, &max, &min);
 	printf("the summary of l2 is: %d\nmin is: %d\nmax is %d", sum, min, max);
-	delete_list(l1);
-	delete_list(l2);
+	delete_list(&l1);
+	delete_list(&l2);
 }
+
